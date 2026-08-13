4d8474ca7510fb0ca8bf33af9e99ada3e2dd2bb2
RayhaanA <rayhaana@google.com>
2026-08-12 22:31:18 -0400

    Update build.gradle.kts with kotlinx.coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,10 @@ dependencies {
   implementation(libs.androidx.compose.material.icons.extended)
   implementation(libs.androidx.navigation.compose)
   implementation(libs.androidx.browser)
+  implementation(libs.kotlinx.coroutines.core)
+  implementation(libs.kotlinx.coroutines.android)
+  implementation(libs.kotlinx.coroutines.play.services)
+
   // Home API SDK dependency:
   implementation(libs.play.services.home)
   implementation(libs.play.services.home.types)
